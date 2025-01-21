288e4e44e2fc9d1bcf9f729d4346658f035c08eb
Dawid Tomasz Kraszkiewicz <39964989+DawidKraszkiewicz@users.noreply.github.com>
2025-01-21 17:49:30 +0100

    Update random_tools.h

--- random_tools.h
+++ random_tools.h
@@ -1,10 +1,6 @@
 #ifndef RANDOM_TOOLS_H
 #define RANDOM_TOOLS_H
 
-/*
-  Deklaracja funkcji do tasowania tablicy liczb całkowitych (Fisher-Yates).
-*/
-
 void shuffle_array(int *array, int n);
 
 #endif
