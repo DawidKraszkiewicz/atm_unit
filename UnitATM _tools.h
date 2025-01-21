327237e0a78e931aa55b258ea922338663b03aaf
Dawid Tomasz Kraszkiewicz <39964989+DawidKraszkiewicz@users.noreply.github.com>
2025-01-21 17:45:37 +0100

    Delete UnitATM _tools.h

--- UnitATM _tools.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNITATM_TOOLS_H
-#define UNITATM_TOOLS_H
-
-int parse_coins(const char *coins_str, int *coins);
-void start_atm(int scale, int *coins, int descriptor, double delay, int seed);
-
-#endif
